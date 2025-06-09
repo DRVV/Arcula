1e00d33277d4f8db760de6bbd4b69878c8161204
ns <n.shimode.drw@gmail.com>
2025-06-09 13:55:28 +0900

    DEV: introduce label node for year label

---
+++ timeline-visualizer/src/components/DateLabelNode.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { format } from 'date-fns';
+
+interface DateLabelNodeProps {
+  data: {
+    date: Date;
+    label: string;
+  };
+}
+
+export default function DateLabelNode({ data }: DateLabelNodeProps) {
+  return (
+    <div className="date-label-node pointer-events-none select-none">
+      <div className="text-center">
+        <span className="text-sm font-semibold text-gray-300 whitespace-nowrap">
+          {data.label}
+        </span>
+      </div>
+    </div>
+  );
+}
